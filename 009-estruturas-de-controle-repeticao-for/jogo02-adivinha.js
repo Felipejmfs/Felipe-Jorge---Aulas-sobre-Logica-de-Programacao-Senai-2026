88db83fcc3958d978b21b53c47f4d719864b20f5
Aluno SENAI <Aluno@SN-378058.edu.fiesc.com.br>
2026-04-27 21:51:20 -0300

    Atividades

--- 009-estruturas-de-controle-repeticao-for/jogo02-adivinha.js
+++ 009-estruturas-de-controle-repeticao-for/jogo02-adivinha.js
@@ -68,6 +68,9 @@ console.log("_______________________________");
 // → Seu código do MODO 1 aqui:
 
 
+
+
+
 // ============================================================
 // MODO 2 – Jogador pensa, computador adivinha
 // ============================================================
@@ -88,4 +91,84 @@ console.log("_______________________________");
 // PASSO 5 – Se o computador não acertar, exiba uma mensagem de derrota e o número que o jogador estava pensando.
 
 // Implemente seu código abaixo:
+
+
+switch (modo) {
+    case 1:
+        let numeroPc = (Math.floor(Math.random() * 100) + 1).toFixed(0)
+        let tentativasUsadas = 0
+        let resultado = "Derrota"
+        console.log(`Computador pensa, você adivinha
+            O computador sorteia um número de 1 a 100.
+            O jogador tem até 7 tentativas para descobrir qual é.
+            A cada erro, o programa diz se o palpite foi alto ou baixo.`);
+            
+        for (let tentativas = 7; tentativas > 0; tentativas--){
+            let palpite = lerTeclado.question(`Digite o seu palpite: `)
+            if (tentativas == 0 && palpite != numeroPc) {
+                console.log(`Fim de jogo! O número era ${numeroPc}.`)
+                process.exit()
+            }
+            if (palpite != numeroPc){
+                if (palpite > numeroPc) {
+                    console.log(`Muito alto! Tente novamente. Restam ${tentativas - 1} tentativas.`)
+                }
+                else console.log(`Muito baixo! Tente novamente. Restam ${tentativas - 1} tentativas.`)
+            } else {
+                console.log(`Parabéns! Você acertou em ${tentativasUsadas + 1} tentativa(s)!`)
+                resultado = "Vitória"
+            }
+            if (palpite == numeroPc){
+                process.exit()
+            }
+            tentativasUsadas++
+        }
+        console.log(`
+        Número secreto: ${numeroPc}
+        Tentativas usadas: ${tentativasUsadas}
+        Resultado: ${resultado}
+        `);
+        break;
+        case 2:
+            let tentativasPc = 0
+            let resultadoPc = "Derrota"
+            let numeroJogador = lerTeclado.questionInt(`Digite um número de 1 a 100: `)
+            let palpitePc = NUM_MAX / 2
+            for (let i = 1; i < 8; i++){
+                console.log(`Tentativa ${i}: Meu palpite é ${Math.ceil(palpitePc)}`);
+                let respostaJogador = lerTeclado.questionInt("1 . Acertei | 2 . Muito alto | 3 . Muito baixo: ")
+                switch (respostaJogador) {
+                    case 1:
+                        console.log(`O computador acertou em ${i} tentativas.`);
+                        process.exit()
+                        break;
+                    case 2:
+                        palpitePc /= 2
+                        break;
+                    case 3:
+                    palpitePc *= 1,5
+                        break;
+                }
+            }
+        break;
+        case 3:
+        console.log("Encerrando.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
