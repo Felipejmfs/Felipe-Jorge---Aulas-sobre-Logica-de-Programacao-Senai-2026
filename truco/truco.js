feaa3748a16c10d385acdd00f5503ad9a0e1089d
FELIPE JORGE MEDEIROS FLOR DA SILVA <felipe_jm_silva@estudante.sesisenai.org.br>
2026-06-09 21:49:59 -0300

    Aprendendo a usar api

--- truco/truco.js
+++ truco/truco.js
@@ -1,10 +1,24 @@
-const baralho = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
-async function chamarApiBaralho() {
-    const baralhoEmbaralhado = await fetch(baralho)
-    console.log(baralhoEmbaralhado);
-    if (baralhoEmbaralhado.status === 200){
-        const obj = await baralhoEmbaralhado.json();
+import axios from 'axios';
+let id = null
+
+async function embaralhar() {
+    try {
+        const baralho = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        console.log(baralho.data.deck_id);
+        id = baralho.data.deck_id
+    } catch (error) {
+        console.error(error);
     }
+
 }
 
-chamarApiBaralho()
+async function darCartas() {
+    try {
+        const darCartas = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`)
+        console.log(darCartas.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+embaralhar()
+darCartas() 
